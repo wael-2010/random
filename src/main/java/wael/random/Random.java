1f2138c36074b57cd3cbf720025ecd8abc702f6e
wael-2010 <rainyxd321@gmail.com>
2026-09-22 07:16:17 +0300

    Initial commit

---
+++ src/main/java/wael/random/Random.java
@@ -0,0 +1,20 @@
+package wael.random;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Random implements ModInitializer {
+	public static final String MOD_ID = "random";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/wael/random/mixin/RandomClient.java
@@ -0,0 +1,10 @@
+package wael.random.mixin;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RandomClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
